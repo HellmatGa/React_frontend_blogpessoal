0af8154ca34c01edefb90e2d54a2880dbeaa1b39
Gabriel Arruda <hellmatgabriel@icloud.com>
2024-06-21 13:27:24 -0300

    Criado documento HOME.tsx e estilização no css

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import homeLogo from '../../assets/home.jpg'
+
+const Home = () => {
+  return (
+    <>
+         <h1 className='titulo'>Home</h1>
+         <img src={homeLogo} className='img' alt="Imagem tela inicial" />
+    </>
+  )
+}
+
+export default Home
